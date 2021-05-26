dabc325e26bb5cd0689612abcfe09fc3097f185f
PIRIS RUIZ <x442898@santanderglobaltech.com>
2021-05-26 15:25:46 +0200

    san image

---
+++ lib/san_image.dart
@@ -0,0 +1,120 @@
+library san_theme;
+
+import 'package:flutter/cupertino.dart';
+
+/*
+  Class to define the styles of whole application.
+  @date: 25-05-2021
+  @author: Jorge Piris Ruiz
+
+*/
+
+class SanImage {
+  SanImage._();
+
+  static const Image agent_in_shirt_with_headset =
+      Image(image: AssetImage('images/3016agent-in-shirt-with-headset.jpg'));
+
+  static const Image tire_in_snow =
+      Image(image: AssetImage('images/5270tire-in-snow.jpg'));
+
+  static const Image man_and_woman_working_in_the_office = Image(
+      image: AssetImage('images/5984man-and-woman-working-in-the-office.jpg'));
+
+  static const Image strong_man_swim_on_ashpalt_road = Image(
+      image: AssetImage('images/6247strong-man-swim-on-asphalt-road.jpg'));
+
+  static const Image people_at_meeting =
+      Image(image: AssetImage('images/7054people-at-meeting.jpg'));
+
+  static const Image futuristic_robot_hand_for_a_handshake = Image(
+      image: AssetImage(
+          'images/11238futuristic-robotic-hand-for-a-handshake.jpg'));
+
+  static const Image young_man_using_browsing_smarthphone = Image(
+      image: AssetImage('images/13612young-man-using-browsing-smartphone.jpg'));
+
+  static const Image successful_business_group_of_people_at_work = Image(
+      image: AssetImage(
+          'images/14638successful-business-group-of-people-at-work.jpg'));
+
+  static const Image sporty_man_look_at_his_smartwatch = Image(
+      image:
+          AssetImage('images/19402sporty-man-looking-at-his-smartwatch.jpg'));
+
+  static const Image female_hands_making_use_of_tablet = Image(
+      image: AssetImage('images/22847female-hands-making-use-of-tablet.jpg'));
+
+  static const Image business_people_signing_a_contract = Image(
+      image: AssetImage('images/25387business-people-signing-a-contract.jpg'));
+
+  static const Image man_measuring_with_yellow_meassure_tape = Image(
+      image: AssetImage(
+          'images/25535man-measuring-with-yellow-meassure-tape.jpg'));
+
+  static const Image young_redhead_woman_talking_on_phone = Image(
+      image:
+          AssetImage('images/26612young-redhead-woman-talking-on-phone.jpg'));
+
+  static const Image caucassian_businesswoman_working_with_papers = Image(
+      image: AssetImage(
+          'images/27489caucasian-businesswoman-working-with-papers.jpg'));
+
+  static const Image young_trendy_hipster_woman =
+      Image(image: AssetImage('images/35383a-young-trendy-hipster-woman.jpg'));
+
+  static const Image business_metting =
+      Image(image: AssetImage('images/38592business-meeting.jpg'));
+
+  static const Image caucassian_call_center_woman_help_desk_service = Image(
+      image: AssetImage(
+          'images/42003caucasian-call-center-woman-help-desk-service.jpg'));
+
+  static const Image start_up_business_team =
+      Image(image: AssetImage('images/42236start-up-business-team.jpg'));
+
+  static const Image people_joined_hand_together =
+      Image(image: AssetImage('images/43861people-joined-hand-together.jpg'));
+
+  static const Image meeting_room =
+      Image(image: AssetImage('images/48972meeting-room.jpg'));
+
+  static const Image happy_business_people_in_office =
+      Image(image: AssetImage('49539happy-business-people-in-office.jpg'));
+
+  static const Image two_business_people_with_wheelchair_and_vr = Image(
+      image: AssetImage(
+          'images/50016two-business-people-with-wheelchair-and-vr.jpg'));
+
+  static const Image caucasian_woman_with_a_smartphone = Image(
+      image: AssetImage('images/52049caucasian-woman-with-a-smartphone.jpg'));
+
+  static const Image male_manager_presenting_at_informal_meeting = Image(
+      image: AssetImage(
+          'images/62403male-manager-presenting-at-informal-meeting.jpg'));
+
+  static const Image busy_person_writing_maby_sticky_notes = Image(
+      image:
+          AssetImage('images/66165busy-person-writing-many-sticky-notes.jpg'));
+
+  static const Image doctor_hands_with_prescription_at_clinic = Image(
+      image: AssetImage(
+          'images/74213doctor-hands-with-prescription-at-clinic.jpg'));
+
+  static const Image startup_business_team_brainstorming = Image(
+      image: AssetImage('images/79455startup-business-team-brainstorming.jpg'));
+
+  static const Image friends_holding_hands_together = Image(
+      image: AssetImage('images/82661friends-holding-hands-together.jpg'));
+
+  static const Image colleagues_working_together_in_modern_office = Image(
+      image: AssetImage(
+          'images/83602colleagues-working-together-in-modern-office.jpg'));
+
+  static const Image business_team_giving_group_high_five_in_park = Image(
+      image: AssetImage(
+          'images/86479business-team-giving-group-high-five-in-park.jpg'));
+
+  static const Image businessman_entrepeneur_motivation = Image(
+      image: AssetImage('images/97075businessman-enterpreneur-motivation.jpg'));
+}
